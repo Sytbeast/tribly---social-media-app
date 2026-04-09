330061ebcbf94650400cbe2b3fe23b674d35b990
vrishabh Kashyap <vrishabh2468@gmail.com>
2026-04-09 09:54:12 +0530

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,  // ← add karo
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
